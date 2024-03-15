bee0d5fa3ed863de92ce4a128333784f0067295c
Dhruv Patel <60015245+Dhruvpatel2491@users.noreply.github.com>
2024-03-15 03:53:51 -0400

    Part2 1,2 Done

--- Programming Language Concepts/C# Programming/P1/part2a_cs_prog_ass.cs
+++ Programming Language Concepts/C# Programming/P1/part2a_cs_prog_ass.cs
@@ -1,109 +1,212 @@
 using System;
 
-
-  // 1. Write a function
-
-  //    public static bool equals(number a, number b)
-
-  //  that returns true if two "numbers" are equal.  For example:
-  //  a rational(1,5) is "equal" to a real(0.2).  However, when making
-  //  calculations avoid loss of precision as much as possible: do not convert
-  //  the rational to a double.  Instead, just cross-multiply: 1/5 = 0.2
-  //  because 1.0 == 0.2*5.  Your function must work for all cases of number.
-  //  You may try to use some of the functions already defined in the abstract
-  //  class, or not ...
-
-  //  /////// Basic math facts:
-
-  //   rules of rational arithmetic:
-  //   a/b + c/d = (a*d+c*b)/(b*d)
-  //   a/b * c/d = (a*c)/(b*d)
-  //   a/b==c/d  if a*d==b*c
-  //   an integer n can be cast to a rational as n/1
-
-  //   rules of complex arithmetic:
-  //   (a+bi) + (c+di) = (a+c) + (b+d)i
-  //   (a+bi) * (c+di) = (ac-bd) + (bc+ad)i  
-  //   a+bi == c+di  if  a==c and b==d.  Complex inequalities don't exit.
-  //   (So -1+0i * c+di = -c + -1*i = -c-i)
-  //   A real number r can be cast to a complex r+0i.
-
-  //   Note that a complex number can be "equal" to another type of number
-  //   only if its imaginary part is zero.
-
 public class arithmetics{
-public static bool equals(number a, number b)
-{
+  public static bool equals(number a, number b)
+  {
 
-    if (a is real) 
-      a=a.upcast(); // A => Complex
-    else if (a is integer)
-      a=a.upcast(); // A => Rational
+          if (a is real)     a=a.upcast(); // A => Complex
+      else if (a is integer)  a=a.upcast(); // A => Rational
 
-    if (b is real) 
-      b=b.upcast(); // B =>Complex
-    else if (b is integer)
-      b=b.upcast(); // B=> Rational
+          if (b is real)     b=b.upcast(); // B =>Complex
+      else if (b is integer)  b=b.upcast(); // B=> Rational
 
 
-    if (a is null || b is null) return false;
+      if (a is null || b is null)    return false;
 
 
-    if (a is rational raA) {
+      if (a is rational raA) {
 
-           if (b is rational raB)  return raA.n==raB.n && raA.d==raB.d;
+            if (b is rational raB)  return raA.n==raB.n && raA.d==raB.d;
 
-      else if (b is complex cB)    return (new complex(raA.n/raA.d,0).r == cB.r) && (cB.i == 0);
-    }
+        else if (b is complex cB)    return (new complex(raA.n/raA.d,0).r == cB.r) && (cB.i == 0);
+      }
 
-    else if (a is complex cA) {
+      else if (a is complex cA) {
 
-           if (b is rational raB)  return (new complex(raB.n/raB.d,0).r == cA.r) && (cA.i == 0);
+            if (b is rational raB)  return (new complex(raB.n/raB.d,0).r == cA.r) && (cA.i == 0);
 
-      else if (b is complex cB)    return (cA.r == cB.r) && (cA.i == cB.i);
-    }
+        else if (b is complex cB)    return (cA.r == cB.r) && (cA.i == cB.i);
+      }
+    
+    return false;
+          
+  }
   
-  return false;
-        
+public static number add(number a, number b){
+
+  if (a is null || b is null)    return new integer(0);
+    a = a.upcast();
+    b = b.upcast();
+    return a.match2_commute(
+        b,
+        II: (ia, ib) => new integer(ia.val + ib.val),
+        IF: (ia, fb) => new rational(ia.val * fb.d + fb.n, fb.d),
+        IR: (ia, rb) => new real(ia.val + rb.val).upcast(),
+        IC: (ia, cb) => new complex(ia.val + cb.r, cb.i),
+        FF: (fa, fb) => new rational(fa.n * fb.d + fb.n * fa.d, fa.d * fb.d),
+        FR: (fa, rb) => new real(fa.n * 1 + rb.val * fa.d).upcast(),
+        FC: (fa, cb) => new complex(fa.n + cb.r / fa.d, cb.i),
+        RR: (ra, rb) => new real(ra.val+rb.val),
+        RC: (ra, cb) => new complex(ra.val + cb.r, cb.i),
+        CC: (ca, cb) => new complex(ca.r + cb.r, ca.i + cb.i)
+        );
+
+}
+
+public static number multiply(number a, number b){
+
+  if (a is null || b is null)    return new integer(0);
+    a = a.upcast();
+    b = b.upcast();
+
+    return a.match2_commute(
+        b,
+        II: (ia, ib) => new integer(ia.val * ib.val),
+        IF: (ia, fb) => new rational(ia.val * fb.n, fb.d),
+        IR: (ia, rb) => new real(ia.val * rb.val).upcast(),
+        IC: (ia, cb) => new complex(ia.val * cb.r, ia.val * cb.i),
+        FF: (fa, fb) => new rational(fa.n * fb.n, fa.d * fb.d),
+        FR: (fa, rb) => new real((fa.n * rb.val)/ fa.d),
+        FC: (fa, cb) => new complex((fa.n/fa.d) * cb.r , (fa.n/fa.d)*cb.i),
+        RR: (ra, rb) => new real(ra.val*rb.val),
+        RC: (ra, cb) => new complex(ra.val * cb.r,ra.val * cb.i),
+        CC: (ca, cb) => new complex((ca.r*cb.r)-(ca.i*cb.i), (ca.i*cb.r)+(ca.r*cb.i))
+    );
 }
 
 
+
+
 }
 
+public class testing{
 
-
-public class part2a_cs_prog_ass{
-  public static void Main() {
+  public static void equal_testing(){
+    //Equal Function testing 
     int n=5;
     number i   = new integer(n);
     number re     = new real(n/1.0);
     number ra = new rational(n,1);
     number c   = new complex(n,1); 
-
-    // Console.WriteLine();
+    Console.WriteLine("----------------");    
     Console.WriteLine(arithmetics.equals(i,i));
     Console.WriteLine(arithmetics.equals(i,re));
     Console.WriteLine(arithmetics.equals(i,ra));
     Console.WriteLine(arithmetics.equals(i,c));
-Console.WriteLine("================");
-
+    Console.WriteLine("----------------");
     Console.WriteLine(arithmetics.equals(ra,i));
     Console.WriteLine(arithmetics.equals(ra,re));
     Console.WriteLine(arithmetics.equals(ra,ra));
     Console.WriteLine(arithmetics.equals(ra,c));
-
-Console.WriteLine("================");
+    Console.WriteLine("----------------");
     Console.WriteLine(arithmetics.equals(re,i));
     Console.WriteLine(arithmetics.equals(re,re));
     Console.WriteLine(arithmetics.equals(re,ra));
     Console.WriteLine(arithmetics.equals(re,c));
-
-Console.WriteLine("================");
-
+    Console.WriteLine("----------------");
     Console.WriteLine(arithmetics.equals(c,i));
     Console.WriteLine(arithmetics.equals(c,re));
     Console.WriteLine(arithmetics.equals(c,ra));
     Console.WriteLine(arithmetics.equals(c,c));
+    Console.WriteLine("----------------");
+  }
+
+  public static void add_testing(){
+    int a=5,b=20;
+    number ia   = new integer(a);
+    number rea     = new real(a/1.0);
+    number raa = new rational(a,1);
+    number ca   = new complex(a,5); 
+
+    number ib   = new integer(b);
+    number reb     = new real(b/1.0);
+    number rab = new rational(b,1);
+    number cb   = new complex(b,10); 
+    Console.WriteLine("ia="+(ia.ToString()));
+    Console.WriteLine("ib="+(ib.ToString()));
+    Console.WriteLine("rea="+(rea.ToString()));
+    Console.WriteLine("reb="+(reb.ToString()));
+    Console.WriteLine("raa="+(raa.ToString()));
+    Console.WriteLine("rab="+(rab.ToString()));
+    Console.WriteLine("ca="+(ca.ToString()));
+    Console.WriteLine("cb="+(cb.ToString()));
+    Console.WriteLine("----------------");    
+    Console.WriteLine(arithmetics.add(ia,ib));
+    Console.WriteLine(arithmetics.add(ia,reb));
+    Console.WriteLine(arithmetics.add(ia,rab));
+    Console.WriteLine(arithmetics.add(ia,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.add(raa,ib));
+    Console.WriteLine(arithmetics.add(raa,reb));
+    Console.WriteLine(arithmetics.add(raa,rab));
+    Console.WriteLine(arithmetics.add(raa,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.add(rea,ib));
+    Console.WriteLine(arithmetics.add(rea,reb));
+    Console.WriteLine(arithmetics.add(rea,rab));
+    Console.WriteLine(arithmetics.add(rea,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.add(ca,ib));
+    Console.WriteLine(arithmetics.add(ca,reb));
+    Console.WriteLine(arithmetics.add(ca,rab));
+    Console.WriteLine(arithmetics.add(ca,cb));
+    Console.WriteLine("----------------");
+  }
+
+    public static void multiply_testing(){
+    int a=5,b=20;
+    number ia   = new integer(a);
+    number rea     = new real(a/1.0);
+    number raa = new rational(a,1);
+    number ca   = new complex(a,2); 
+
+    number ib   = new integer(b);
+    number reb     = new real(b/1.0);
+    number rab = new rational(b,1);
+    number cb   = new complex(b,15); 
+    Console.WriteLine("ia="+(ia.ToString()));
+    Console.WriteLine("ib="+(ib.ToString()));
+    Console.WriteLine("rea="+(rea.ToString()));
+    Console.WriteLine("reb="+(reb.ToString()));
+    Console.WriteLine("raa="+(raa.ToString()));
+    Console.WriteLine("rab="+(rab.ToString()));
+    Console.WriteLine("ca="+(ca.ToString()));
+    Console.WriteLine("cb="+(cb.ToString()));
+    Console.WriteLine("----------------");    
+    Console.WriteLine(arithmetics.multiply(ia,ib));
+    Console.WriteLine(arithmetics.multiply(ia,reb));
+    Console.WriteLine(arithmetics.multiply(ia,rab));
+    Console.WriteLine(arithmetics.multiply(ia,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.multiply(raa,ib));
+    Console.WriteLine(arithmetics.multiply(raa,reb));
+    Console.WriteLine(arithmetics.multiply(raa,rab));
+    Console.WriteLine(arithmetics.multiply(raa,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.multiply(rea,ib));
+    Console.WriteLine(arithmetics.multiply(rea,reb));
+    Console.WriteLine(arithmetics.multiply(rea,rab));
+    Console.WriteLine(arithmetics.multiply(rea,cb));
+    Console.WriteLine("----------------");
+    Console.WriteLine(arithmetics.multiply(ca,ib));
+    Console.WriteLine(arithmetics.multiply(ca,reb));
+    Console.WriteLine(arithmetics.multiply(ca,rab));
+    Console.WriteLine(arithmetics.multiply(ca,cb));
+    Console.WriteLine("----------------");
+  }
+
+}
+
+public class part2a_cs_prog_ass{
+  public static void Main() {
+    Console.WriteLine("========1.EQUAL========"); 
+    testing.equal_testing();
+    Console.WriteLine("========2.ADD========"); 
+    testing.add_testing();
+    Console.WriteLine("========2.Multiply========"); 
+    testing.multiply_testing();
+
 
   }
 }
